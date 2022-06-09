4673d6ccc74e8d8525bcc382efc73d4df34a2e7a
MountainGator <luketaylorsanders@gmail.com>
2022-06-08 20:53:39 -0500

    solve rest of exercises

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -7,14 +7,34 @@ SELECT category, COUNT(*) FROM analytics GROUP BY category;
 -- question 4 --
 SELECT app_name, rating, reviews FROM analytics WHERE rating IS NOT null ORDER BY rating desc LIMIT 5;
 -- question 5 --
-
+SELECT app_name, rating, reviews FROM analytics WHERE rating > 4.8 ORDER BY reviews desc LIMIT 10;
 -- question 6 --
+SELECT category, AVG(rating) AS avg_rating FROM analytics GROUP BY category ORDER BY avg_rating desc;
 -- question 7 --
+SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price desc LIMIT 5;
 -- question 8 --
+SELECT app_name, rating, min_installs FROM analytics WHERE rating IS NOT null AND min_installs <= 50;
 -- question 9 --
+SELECT app_name, rating, reviews FROM analytics WHERE rating < 3 AND reviews >= 10000;
 -- question 10 --
+SELECT app_name, rating, reviews, price 
+    FROM analytics 
+    WHERE price BETWEEN 0.10 AND 1 
+    ORDER BY reviews desc 
+    LIMIT 10;
 -- question 11 --
+SELECT app_name, last_updated 
+    FROM analytics 
+    ORDER BY last_updated 
+    LIMIT 10;
 -- question 12 --
+SELECT app_name, price FROM analytics ORDER BY price desc LIMIT 10;
 -- question 13 --
+SELECT SUM(reviews) FROM analytics;
 -- question 14 --
+SELECT category, COUNT(*) FROM analytics GROUP BY category HAVING COUNT(*) > 300;
 -- question 15 --
+SELECT app_name, reviews, min_installs, min_installs / reviews AS proportion 
+    FROM analytics
+    WHERE min_installs > 100000
+    ORDER BY proportion desc;
